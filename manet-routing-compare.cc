5e38c2ac85ebd0400b8eb30ef11808471ad88c77
sjamga zhou <984596004@qq.com>
2022-10-17 17:18:27 +0800

    s

--- manet-routing-compare.cc
+++ manet-routing-compare.cc
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *1111111111111111
+ *1111111111111111weewweqqwe
  * Author: Justin Rohrer <rohrej@ittc.ku.edu>
  *
  * James P.G. Sterbenz <jpgs@ittc.ku.edu>, director
